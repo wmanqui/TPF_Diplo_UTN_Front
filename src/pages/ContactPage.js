4ff17d61ca89034e1c47022cf666072ea4e7ab69
Walter Manqui <wmanqui@gmail.com>
2023-11-27 20:25:25 -0300

    Se modifico el codigo para el envio de mails

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,43 +1,69 @@
-/*
 import React from "react";
-
-const ContactPage = (prompt) =>{
-    return(
-        <div>ContactPage</div>
-    );
-}
-
-export default ContactPage;
-*/
-
-import React from "react";
+//Se utiliza estas dependencias para controlar el formulario,
+//el feedback del mismo y realizar el envio de los datos cargados
+//por el usuario hacia la api.
+import { useState } from "react";
+import axios from "axios";
 
 const ContactPage = (props) => {
+
+ //Inicializo las variables del formulario con todos sus valores vacios. 
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    }
+    const [sending, setSending] = useState(false); //encargada de mostrar o no al usuario el estado de "enviando..."
+    const [msg, setMsg] = useState(''); //almacena y muestra el msj recibido por la API para indicar al usuario que su información fue enviada
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e =>{
+        const { name, value } = e.target;
+        setFormData(oldData =>({
+               oldData,
+            [name]: value //forma dinamica
+        }));
+    }
+
+    const handleSubmit = async e => { //Controla el envio de los datos y la actualización de las variables de estado que controlan el feedback que el usuario recibe.
+        e.preventDefault();
+        setMsg('');
+        setSending(true)
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if(response.data.error === false){
+            setFormData(initialForm)
+        }
+    }
+
     return(
         <main className="holder contacto">
             <div>
                 <h2>Contacto Rapido</h2>
-                <form>
+                <form className="formulario" onSubmit={handleSubmit}>
                     <p>
-                        <label for = 'nombre'>Nombre</label>
-                        <input type = 'text' name=''/>
+                        <label>Nombre</label>
+                        <input type='text' name='nombre' value={formData.nombre} onChange={handleChange}/>
                     </p>
                     <p>
-                        <label for = 'email'>Email</label>
-                        <input type = 'text' name=''/>
+                        <label>Email</label>
+                        <input type='text' name='email' value={formData.email} onChange={handleChange}/>
                     </p>
                     <p>
-                        <label for = 'telefono'>Telefono</label>
-                        <input type = 'text' name=''/>
+                        <label>Telefono</label>
+                        <input type='text' name='telefono' value={formData.telefono} onChange={handleChange}/>
                     </p>
                     <p>
-                        <label for = 'mensaje'>Mensaje</label>
-                        <textarea name=''></textarea>
+                        <label>Comentario</label>
+                        <textarea name='mensaje' value={formData.mensaje} onChange={handleChange}></textarea>
                     </p>
-                    <p class="acciones">
+                    {sending? <p>Enviando...</p>: null}
+                    {msg? <p>{msg}</p> : null}
+                        <p className="centrar">
                         <input type='submit' value='Enviar'/>
-
-                    </p>
+                        </p>
                 </form>
             </div>
         </main>
